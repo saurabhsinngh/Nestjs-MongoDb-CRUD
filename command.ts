fa03136b542d34aaee8a14ce244fa85a9406486f
Saurabh Singh <saurabh28720@gmail.com>
2025-06-26 01:42:57 +0530

    mongodb connection establised

---
+++ command.ts
@@ -0,0 +1,12 @@
+// To install the nestjs cli globly if not run use sudo: npm i -g @nestjs/cli
+// To Generate new nest project: nest new .     (Here dot is give the current folder name)
+
+// To Generate new nest project: nest new project_or_projectfolderName
+// To install mongoose dependency: npm i @nestjs/mongoose mongoose
+// To Start The Application by command: npm run start:dev
+
+// Now to make the Model, controller & service.
+// To make model: nest g module book
+// To make controller: nest g controller book
+// To make service: nest g service book --no-spec  (Here --no-spec is use for not to generate the test file)
+// To install the depenedency: npm i mongoose @nestjs/mongoose @nestjs/config --save
