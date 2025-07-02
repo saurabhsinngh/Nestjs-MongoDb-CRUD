2697549c336221c102e1c6501a4f222bcab0075d
Saurabh Singh <saurabh28720@gmail.com>
2025-07-02 23:59:21 +0530

    Add data validator

--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,9 +1,26 @@
+import { IsEnum, IsNotEmpty, IsNumber, IsString } from 'class-validator';
 import { CATEGORY } from '../chemas/book.schema';
 
 export class CreateBookDto {
+
+    @IsNotEmpty()
+    @IsString()
     readonly author: string;
+    
+    @IsNotEmpty()
+    @IsString()
     readonly description: string;
+    
+    @IsNotEmpty()
+    @IsString()
     readonly title: string;
+
+    
+    @IsNotEmpty()
+    @IsNumber()
     readonly price: number;
+
+    @IsNotEmpty()
+    @IsEnum(CATEGORY, { message: 'Please enter the correct cotegory'})
     readonly category: CATEGORY;
 }
--- src/books/dto/update-book.dto.ts
+++ src/books/dto/update-book.dto.ts
@@ -1,9 +1,25 @@
+import { IsEnum, IsNumber, IsOptional, IsString } from 'class-validator';
 import { CATEGORY } from '../chemas/book.schema';
 
 export class UpdateBookDto {
+
+    @IsOptional()    
+    @IsString()
     readonly author: string;
+
+    @IsOptional()    
+    @IsString()
     readonly description: string;
+
+    @IsOptional()    
+    @IsString()
     readonly title: string;
+
+    @IsOptional()    
+    @IsNumber()
     readonly price: number;
+
+    @IsOptional()    
+    @IsEnum(CATEGORY, { message: 'Please enter the correct cotegory'})
     readonly category: CATEGORY;
 }
