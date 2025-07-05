b64735d3c28ededed81b37842179100cb085f56c
Saurabh Singh <saurabh28720@gmail.com>
2025-07-05 23:58:58 +0530

    Add the user schema

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from 'src/schamas/user.schema';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt'}),
+    MongooseModule.forFeature([{name: 'User', schema: UserSchema}])
+  ],
   controllers: [AuthController],
   providers: [AuthService]
 })
--- src/schamas/user.schema.ts
+++ src/schamas/user.schema.ts
@@ -1,9 +1,18 @@
-import { Schema } from "@nestjs/mongoose";
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 
 @Schema({
     timestamps: true
 })
 
-export class User{
+export class User {
+    @Prop()
+    name: string
 
+    @Prop({ unique: [true, 'Invalid email is entered'] })
+    email: string
+
+    @Prop()
+    password: string
 }
+
+export const UserSchema = SchemaFactory.createForClass(User);
