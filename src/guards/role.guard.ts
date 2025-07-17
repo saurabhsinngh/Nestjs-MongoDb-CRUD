c19091d0347898a90fcc093277ca2502aab6f239
Saurabh Singh <saurabh28720@gmail.com>
2025-07-17 23:58:59 +0530

    Add changes in the roles

--- src/guards/role.guard.ts
+++ src/guards/role.guard.ts
@@ -1,7 +1,20 @@
-import { CanActivate, Injectable } from "@nestjs/common";
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { Observable } from "rxjs";
+import { ROLES_KEY } from "src/decorators/role.decorator";
+import { Role } from "src/enums/role.enum";
 
 
 @Injectable()
-export class RolesGuards implements CanActivate{
+export class RolesGuards implements CanActivate {
+    constructor(private reflector: Reflector){}
+
+    canActivate(context: ExecutionContext): boolean {
+        const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+            context.getHandler(),
+            context.getClass()
+        ])
+        
+    }
     
 }
